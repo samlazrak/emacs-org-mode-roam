e80e9db83af97f5d19212a9b18bf8aca7470fe8d
Sam Lazrak <24284329+samlazrak@users.noreply.github.com>
2025-08-05 10:49:27 -0500

    Wow

--- custom.el
+++ custom.el
@@ -4,7 +4,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(elfeed-feeds '("https://hnrss.org/frontpage"))
+ '(elfeed-feeds
+   '("https://fab.cba.mit.edu/classes/MAS.863/"
+     ("https://hnrss.org/frontpage" programming hnfront)
+     ("https://hnrss.org/best" programming hnbest)
+     ("https://this-week-in-rust.org/rss.xml" programming rust)))
  '(elfeed-score-serde-score-file (concat org-directory "/Feeds/elfeed.score") nil nil "Customized with use-package elfeed-score")
  '(package-selected-packages '(obsidian org-web-tools pandoc plantuml-mode vterm)))
 (custom-set-faces
